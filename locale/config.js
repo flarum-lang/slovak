dec29992518a1887145a98d2520250708784f579
Kevo <me@kevo.link>
2022-08-21 19:45:32 +0200

    Fix config.js

--- locale/config.js
+++ locale/config.js
@@ -1,47 +1,45 @@
-// Slovak [sk]
-import dayjs from 'dayjs'
+/* Based on https://github.com/iamkun/dayjs/blob/e3777ed0f8189ad8f3a36001ecc583f74edd81df/src/locale/sk.js */
 
 function plural(n) {
   return (n > 1) && (n < 5) && (~~(n / 10) !== 1) // eslint-disable-line
 }
 
-/* eslint-disable */
 function translate(number, withoutSuffix, key, isFuture) {
   const result = `${number} `
   switch (key) {
-    case 's': // a few seconds / in a few seconds / a few seconds ago
+    case 's':
       return (withoutSuffix || isFuture) ? 'pár sekúnd' : 'pár sekundami'
-    case 'm': // a minute / in a minute / a minute ago
+    case 'm':
       return withoutSuffix ? 'minúta' : (isFuture ? 'minútu' : 'minútou')
-    case 'mm': // 9 minutes / in 9 minutes / 9 minutes ago
+    case 'mm':
       if (withoutSuffix || isFuture) {
         return result + (plural(number) ? 'minúty' : 'minút')
       }
       return `${result}minútami`
-    case 'h': // an hour / in an hour / an hour ago
+    case 'h':
       return withoutSuffix ? 'hodina' : (isFuture ? 'hodinu' : 'hodinou')
-    case 'hh': // 9 hours / in 9 hours / 9 hours ago
+    case 'hh':
       if (withoutSuffix || isFuture) {
         return result + (plural(number) ? 'hodiny' : 'hodín')
       }
       return `${result}hodinami`
-    case 'd': // a day / in a day / a day ago
+    case 'd':
       return (withoutSuffix || isFuture) ? 'deň' : 'dňom'
-    case 'dd': // 9 days / in 9 days / 9 days ago
+    case 'dd':
       if (withoutSuffix || isFuture) {
         return result + (plural(number) ? 'dni' : 'dní')
       }
       return `${result}dňami`
-    case 'M': // a month / in a month / a month ago
+    case 'M':
       return (withoutSuffix || isFuture) ? 'mesiac' : 'mesiacom'
-    case 'MM': // 9 months / in 9 months / 9 months ago
+    case 'MM':
       if (withoutSuffix || isFuture) {
         return result + (plural(number) ? 'mesiace' : 'mesiacov')
       }
       return `${result}mesiacmi`
-    case 'y': // a year / in a year / a year ago
+    case 'y':
       return (withoutSuffix || isFuture) ? 'rok' : 'rokom'
-    case 'yy': // 9 years / in 9 years / 9 years ago
+    case 'yy':
       if (withoutSuffix || isFuture) {
         return result + (plural(number) ? 'roky' : 'rokov')
       }
@@ -49,7 +47,7 @@ function translate(number, withoutSuffix, key, isFuture) {
   }
 }
 /* eslint-enable */
-const locale = {
+dayjs.locale({
   name: 'sk',
   weekdays: 'nedeľa_pondelok_utorok_streda_štvrtok_piatok_sobota'.split('_'),
   weekdaysShort: 'ne_po_ut_st_št_pi_so'.split('_'),
@@ -69,7 +67,7 @@ const locale = {
     l: 'D. M. YYYY'
   },
   relativeTime: {
-    future: 'za %s', // Should be `o %s` (change when moment/moment#5408 is fixed)
+    future: 'o %s',
     past: 'pred %s',
     s: translate,
     m: translate,
@@ -83,8 +81,4 @@ const locale = {
     y: translate,
     yy: translate
   }
-}
-
-dayjs.locale(locale, null, true)
-
-export default locale
+}, null, false)
